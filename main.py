3d0fb35a26f22383c0e7329406e92e5a835fe841
Tech 13-08 <falaktulsi@gmail.com>
2022-04-25 00:59:24 -0700

    Deleted Turtle Import

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from turtle import Turtle
 import pygame
 import numpy as np
 import datetime as dt
@@ -12,61 +11,91 @@ screenW = math.floor(infoObject.current_w * 0.8)
 screenH = math.floor(infoObject.current_h * 0.8)
 
 win = pygame.display.set_mode((screenW, screenH))
-win.fill((250,250,250))
+win.fill((250, 250, 250))
 pygame.display.set_caption("Garden Tank App")
 clock = pygame.time.Clock()
 
 run = True
 
+
 class Map:
-    def __init__(self, height, width,boxSize):
+    def __init__(self, height, width, boxSize):
         self.width = width
         self.height = height
-        self.map_array = np.zeros((height, width),dtype=int)
+        self.map_array = np.zeros((height, width), dtype=int)
         self.water_direction = "left"
         self.box_size = boxSize
         self.path_array = []
-        self.x = math.floor((screenW - (self.box_size * self.width))/2)
-        self.y = math.floor((screenH - (self.box_size * self.height))/2)
-        self.bgC = (211,211,211)
-        self.borderC = (0,0,0)
-        self.plantC = (0,153,0)
-        self.obstacleC = (102,51,0)
-        self.pathC = (255,0,0)
+        self.x = math.floor((screenW - (self.box_size * self.width)) / 2)
+        self.y = math.floor((screenH - (self.box_size * self.height)) / 2)
+        self.bgC = (211, 211, 211)
+        self.borderC = (0, 0, 0)
+        self.plantC = (0, 153, 0)
+        self.obstacleC = (102, 51, 0)
+        self.pathC = (255, 0, 0)
         self.day_to_water = 0
         self.time_to_water = ""
         self.visible = False
 
     def draw(self, surface):
         if self.visible:
-            self.x = math.floor((screenW - (self.box_size * self.width))/2)
-            self.y = math.floor((screenH - (self.box_size * self.height))/2)
-            for j in range (self.map_array.shape[0]):
-                for i in range (self.map_array.shape[1]):
-                    xPos = self.x + (i*self.box_size)
-                    yPos = self.y + (j*self.box_size)
-                    pygame.draw.rect(surface, self.borderC, ((xPos)-1, (yPos)-1,self.box_size+2,self.box_size+2))
+            self.x = math.floor((screenW - (self.box_size * self.width)) / 2)
+            self.y = math.floor((screenH - (self.box_size * self.height)) / 2)
+            for j in range(self.map_array.shape[0]):
+                for i in range(self.map_array.shape[1]):
+                    xPos = self.x + (i * self.box_size)
+                    yPos = self.y + (j * self.box_size)
+                    pygame.draw.rect(
+                        surface,
+                        self.borderC,
+                        ((xPos) - 1, (yPos) - 1, self.box_size + 2, self.box_size + 2),
+                    )
                     if self.map_array[j][i] == 0:
-                        pygame.draw.rect(surface, self.bgC, (xPos, yPos,self.box_size,self.box_size))
+                        pygame.draw.rect(
+                            surface,
+                            self.bgC,
+                            (xPos, yPos, self.box_size, self.box_size),
+                        )
                     if self.map_array[j][i] == 1:
-                        pygame.draw.rect(surface, self.plantC, (xPos, yPos,self.box_size,self.box_size))
+                        pygame.draw.rect(
+                            surface,
+                            self.plantC,
+                            (xPos, yPos, self.box_size, self.box_size),
+                        )
                     if self.map_array[j][i] == 2:
-                        pygame.draw.rect(surface, self.obstacleC, (xPos, yPos,self.box_size,self.box_size)) 
-                    if self.map_array[j][i] == 3:   
-                        pygame.draw.rect(surface, self.bgC, (xPos, yPos,self.box_size,self.box_size))
-                        pygame.draw.circle(surface, self.pathC, ((xPos+(self.box_size//2), yPos+(self.box_size//2))),self.box_size//4)
+                        pygame.draw.rect(
+                            surface,
+                            self.obstacleC,
+                            (xPos, yPos, self.box_size, self.box_size),
+                        )
+                    if self.map_array[j][i] == 3:
+                        pygame.draw.rect(
+                            surface,
+                            self.bgC,
+                            (xPos, yPos, self.box_size, self.box_size),
+                        )
+                        pygame.draw.circle(
+                            surface,
+                            self.pathC,
+                            (
+                                (
+                                    xPos + (self.box_size // 2),
+                                    yPos + (self.box_size // 2),
+                                )
+                            ),
+                            self.box_size // 4,
+                        )
 
     def updateMap(self, pos, block):
-        xPos = ((pos[0]-self.x)//self.box_size)
-        yPos = ((pos[1]-self.y)//self.box_size)
+        xPos = (pos[0] - self.x) // self.box_size
+        yPos = (pos[1] - self.y) // self.box_size
         if xPos >= 0 and xPos < self.width:
             if yPos >= 0 and yPos < self.height:
                 self.map_array[yPos][xPos] = block
                 if block == 3:
-                    self.path_array.append((yPos,xPos))
-                elif (yPos,xPos) in self.path_array:
-                    self.path_array.remove((yPos,xPos))
-    
+                    self.path_array.append((yPos, xPos))
+                elif (yPos, xPos) in self.path_array:
+                    self.path_array.remove((yPos, xPos))
 
 
 class Button:
@@ -114,12 +143,12 @@ class Button:
 
         return False
 
-class InputBox:
 
-    def __init__(self, x, y, w, h, text=''):
+class InputBox:
+    def __init__(self, x, y, w, h, text=""):
         self.rect = pygame.Rect(x, y, w, h)
         self.width = w
-        self.color = (211,211,211)
+        self.color = (211, 211, 211)
         self.defaultText = text
         self.text = ""
         self.font = pygame.font.SysFont("comfortaa", 45)
@@ -136,12 +165,12 @@ class InputBox:
             else:
                 self.active = False
             # Change the current color of the input box.
-            self.color = (211,211,211) if self.active else (0,0,0)
+            self.color = (211, 211, 211) if self.active else (0, 0, 0)
         if event.type == pygame.KEYDOWN:
             if self.active:
                 if event.key == pygame.K_RETURN:
                     print(self.text)
-                    self.text = ''
+                    self.text = ""
                 elif event.key == pygame.K_BACKSPACE:
                     self.text = self.text[:-1]
                 else:
@@ -151,51 +180,121 @@ class InputBox:
 
     def update(self):
         # Resize the box if the text is too long.
-        width = max(self.width, self.txt_surface.get_width()+10)
+        width = max(self.width, self.txt_surface.get_width() + 10)
         self.rect.w = width
 
     def draw(self, screen):
         if self.visible:
             # Blit the text.
-            screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+            screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
             self.update()
             # Blit the rect.
             pygame.draw.rect(screen, self.color, self.rect, 2)
 
+
 def redrawWindow():
-    win.fill((250,250,250))
-    mapEditBtn.draw(win, (0,0,0))
-    inputDimensionsBtn.draw(win, (0,0,0))
-    backBtn.draw(win, (0,0,0))
-    mapDoneBtn.draw(win, (0,0,0))
+    win.fill((250, 250, 250))
+    mapEditBtn.draw(win, (0, 0, 0))
+    inputDimensionsBtn.draw(win, (0, 0, 0))
+    backBtn.draw(win, (0, 0, 0))
+    mapDoneBtn.draw(win, (0, 0, 0))
     test_map.draw(win)
     input_width.draw(win)
     input_height.draw(win)
-    plantBtn.draw(win, (0,0,0))
-    obstacleBtn.draw(win, (0,0,0))
-    eraserBtn.draw(win, (0,0,0))
-    pathBtn.draw(win, (0,0,0))
+    plantBtn.draw(win, (0, 0, 0))
+    obstacleBtn.draw(win, (0, 0, 0))
+    eraserBtn.draw(win, (0, 0, 0))
+    pathBtn.draw(win, (0, 0, 0))
     pygame.display.update()
 
-test_map = Map(0,0,0)
-mapEditBtn = Button((200,200,200),math.floor((screenW/2)-100),math.floor((screenH/2)-25),200,50,"EDIT MAP")
+
+test_map = Map(0, 0, 0)
+mapEditBtn = Button(
+    (200, 200, 200),
+    math.floor((screenW / 2) - 100),
+    math.floor((screenH / 2) - 25),
+    200,
+    50,
+    "EDIT MAP",
+)
 mapEditBtn.visible = True
-inputDimensionsBtn = Button((200,200,200), math.floor((screenW/2)-100), math.floor((screenH/2)+70), 200,50,"ENTER")
-backBtn = Button((200,200,200), math.floor((screenW*0.01)), math.floor((screenH*0.01)), 200,50,"BACK")
-mapDoneBtn = Button((200,200,200), math.floor((screenW/2))-200, math.floor((screenH*0.905)), 400,50,"DONE WITH MAP")
-plantBtn = Button(test_map.plantC, math.floor((screenW/2)-400), math.floor((screenH*0.01)), 200,50,"PLANT")
-obstacleBtn = Button(test_map.obstacleC, math.floor((screenW/2))-190, math.floor((screenH*0.01)), 200,50,"NO ZONE")
-eraserBtn = Button(test_map.bgC, math.floor((screenW/2))+210, math.floor((screenH*0.01)), 200,50,"ERASER")
-pathBtn = Button(test_map.pathC, math.floor((screenW/2))+420, math.floor((screenH*0.01)), 200,50,"PATH")
-input_width = InputBox(math.floor((screenW/2)-200),math.floor((screenH/2)-60),400,50,"ENTER WIDTH IN FEET")
-input_height = InputBox(math.floor((screenW/2)-200),math.floor((screenH/2)+10),400,50,"ENTER HEIGHT IN FEET")
+inputDimensionsBtn = Button(
+    (200, 200, 200),
+    math.floor((screenW / 2) - 100),
+    math.floor((screenH / 2) + 70),
+    200,
+    50,
+    "ENTER",
+)
+backBtn = Button(
+    (200, 200, 200),
+    math.floor((screenW * 0.01)),
+    math.floor((screenH * 0.01)),
+    200,
+    50,
+    "BACK",
+)
+mapDoneBtn = Button(
+    (200, 200, 200),
+    math.floor((screenW / 2)) - 200,
+    math.floor((screenH * 0.905)),
+    400,
+    50,
+    "DONE WITH MAP",
+)
+plantBtn = Button(
+    test_map.plantC,
+    math.floor((screenW / 2) - 400),
+    math.floor((screenH * 0.01)),
+    200,
+    50,
+    "PLANT",
+)
+obstacleBtn = Button(
+    test_map.obstacleC,
+    math.floor((screenW / 2)) - 190,
+    math.floor((screenH * 0.01)),
+    200,
+    50,
+    "NO ZONE",
+)
+eraserBtn = Button(
+    test_map.bgC,
+    math.floor((screenW / 2)) + 210,
+    math.floor((screenH * 0.01)),
+    200,
+    50,
+    "ERASER",
+)
+pathBtn = Button(
+    test_map.pathC,
+    math.floor((screenW / 2)) + 420,
+    math.floor((screenH * 0.01)),
+    200,
+    50,
+    "PATH",
+)
+input_width = InputBox(
+    math.floor((screenW / 2) - 200),
+    math.floor((screenH / 2) - 60),
+    400,
+    50,
+    "ENTER WIDTH IN FEET",
+)
+input_height = InputBox(
+    math.floor((screenW / 2) - 200),
+    math.floor((screenH / 2) + 10),
+    400,
+    50,
+    "ENTER HEIGHT IN FEET",
+)
 map_editing = False
 currentBlock = 0
 print(screenW)
 print(screenH)
 while run:
     clock.tick(27)
-    
+
     # Check Events ----------------------------------------------------------------------------------------------------------------
     for event in pygame.event.get():
         mousePos = pygame.mouse.get_pos()
@@ -209,10 +308,10 @@ while run:
                 mapHeight = int(input_height.text)
                 mapWidth = int(input_width.text)
                 mapBoxSize = 0
-                if (screenH*0.9)/mapHeight > (screenW*0.9)/mapWidth:
-                    mapBoxSize = math.floor((screenW*0.9)/mapWidth)
+                if (screenH * 0.9) / mapHeight > (screenW * 0.9) / mapWidth:
+                    mapBoxSize = math.floor((screenW * 0.9) / mapWidth)
                 else:
-                    mapBoxSize = math.floor((screenH*0.9)/mapHeight)
+                    mapBoxSize = math.floor((screenH * 0.9) / mapHeight)
                 test_map = Map(mapHeight, mapWidth, mapBoxSize)
                 input_width.visible = False
                 input_height.visible = False
@@ -228,7 +327,7 @@ while run:
                     obstacleBtn.visible = False
                     eraserBtn.visible = False
                     pathBtn.visible = False
-                
+
                 if plantBtn.isOver(mousePos):
                     currentBlock = 1
                 if obstacleBtn.isOver(mousePos):
@@ -242,7 +341,6 @@ while run:
                 if mapDoneBtn.isOver(mousePos):
                     print(test_map.path_array)
 
-            
         if map_editing:
             if test_map.width == 0:
                 input_width.visible = True
@@ -259,11 +357,5 @@ while run:
                 eraserBtn.visible = True
                 pathBtn.visible = True
 
-                
-
-            
     redrawWindow()
-
-
-
 
